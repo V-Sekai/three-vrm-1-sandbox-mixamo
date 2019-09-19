708ae376cc06be2974f2b4b8177ab1784417e659
Glitch (hello-webpage) <none>
2019-09-19 05:22:30 +0000

    🏎🙋 Checkpoint ./main.js:5985195/609

--- main.js
+++ main.js
@@ -16,20 +16,20 @@ camera.position.set( 0.0, 0.0, 5.0 );
 const scene = new THREE.Scene();
 
 // -- avocado (gltf) --
-let currentGLTF = undefined;
+let currentGLTF = undefined; // 現在使用中のgltf、update内で使えるようにするため
 
 function initGLTF( gltf ) {
-  scene.add( gltf.scene );
-  gltf.scene.scale.set( 20.0, 20.0, 20.0 );
-  currentGLTF = gltf;
+  scene.add( gltf.scene ); // gltfのモデルをsceneに追加
+  gltf.scene.scale.set( 20.0, 20.0, 20.0 ); // 小さすぎるので大きさを20倍に
+  currentGLTF = gltf; // currentGLTFにgltfを代入
 }
 
 const loader = new THREE.GLTFLoader();
 loader.load(
   'https://cdn.glitch.com/0a36e09b-3913-4217-902a-b2292d472997%2FAvocado.glb?v=1568864419285',
-  ( gltf ) => { initGLTF( gltf ); },
-  ( progress ) => { console.info( 100.0 * progress.loaded / progress.total, '% loaded' ); },
-  ( error ) => { console.error( error ); }
+  ( gltf ) => { initGLTF( gltf ); }, // もでる
+  ( progress ) => { console.info( ( 100.0 * progress.loaded / progress.total ).toFixed( 2 ) + '% loaded' ); }, // モデル読み込みの進捗を表示
+  ( error ) => { console.error( error ); } // モデル読み込み時のエラーを表示
 );
 
 // -- light --
@@ -47,7 +47,6 @@ function update() {
   const delta = clock.getDelta();
 
   if ( currentGLTF ) {
-    currentGLTF.scene.rotation.x += delta;
     currentGLTF.scene.rotation.y += delta;
   }
 
